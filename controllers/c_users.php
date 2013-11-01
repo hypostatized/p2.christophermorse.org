8df3f00cab611e03ab561ecaf6b5935a7aa5d6fc
Christopher Morse <christophermorse@fas.harvard.edu>
2013-11-01 18:34:42 -0400

    forgot semicolon in the c_users file

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -204,7 +204,7 @@ class users_controller extends base_controller {
                         $filename = $_FILES["file"]["tmp_name"];
                         $avatar = Array("avatar" => $filename);
                         DB::instance(DB_NAME)->update("users", $avatar, "WHERE user_id = '".$this->user->user_id."'");
-                        Router::redirect('/users/profile')
+                        Router::redirect('/users/profile');
                     }
                 }
             }
